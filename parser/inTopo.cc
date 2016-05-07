6de8172ab38787757813343057908601d29466b4
Xiaotian Zhang <xiaotiannj@gmail.com>
2016-05-06 21:29:55 -0400

    Added files via upload
    This is a way to make the output of the parser in order to post order, but this is just the current work, it need to be fit in the parser and there is a problem that some symbols like /d /t need to be fix, also this is just the demo, so there would be some bugs need to be fix when it can be applied to the parser's post order output. We need to discuss that.

---
+++ parser/inTopo.cc
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<string>
+#include<stack>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+stack<char> stack_s;
+queue<char> queue_t;
+//vector<char> input_s;
+
+string input(){
+	cout<<"input Inorder: ";
+	string expression;
+	cin>> expression;
+	return expression;
+}
+void change(const char *p){
+	//char e;
+	int flag = 1;
+	while(*p){
+		if((*p >= 'a' && *p<='z') ||(*p>='A'&&*p<='Z')||(*p>='0'&&*p<='9')){
+			queue_t.push(*p);
+			queue_t.push(' ');
+		}
+		else{
+			if(*p == '(' || *p == ')'){
+				if(*p == '(')
+					stack_s.push(*p);
+				else{
+					while(stack_s.top()!='('){
+						queue_t.push(stack_s.top());
+						stack_s.pop();
+						queue_t.push(' ');
+					}
+					stack_s.pop();
+				}
+			}
+			else{
+				if(*p == '*'|| *p == '+' || *p == '?'){
+					if(stack_s.empty()||stack_s.top()=='|'||stack_s.top()=='.'||stack_s.top()=='(')
+						stack_s.push(*p);
+					else{
+						while(!stack_s.empty()&&(stack_s.top()=='*'||stack_s.top()=='+' || stack_s.top() == '?')){//新读入操作符优先级大于S栈顶操作符,S顶操作符出栈,进T队,再和S下一栈顶操作符比较,如此循环
+							queue_t.push(stack_s.top());
+							stack_s.pop();
+							queue_t.push(' ');
+						}
+						stack_s.push(*p);
+					}
+				}
+				else if(*p == '.'){
+					if(stack_s.empty()||stack_s.top() == '|'||stack_s.top()=='(')
+						stack_s.push(*p);
+					else{
+						while(!stack_s.empty()&&((stack_s.top()=='.'||stack_s.top()=='*')||(stack_s.top()=='*'||stack_s.top()=='+'||stack_s.top()=='?'))){
+							queue_t.push(stack_s.top());
+							stack_s.pop();
+							queue_t.push(' ');
+						}
+						stack_s.push(*p);
+					}
+				}
+				else{
+					if(stack_s.empty()||stack_s.top()=='(')
+						stack_s.push(*p);
+					else{
+						while(!stack_s.empty()&&((stack_s.top()=='|'||stack_s.top()=='.')||(stack_s.top()=='*'||stack_s.top()=='+'||stack_s.top()=='?'))){
+							queue_t.push(stack_s.top());
+							stack_s.pop();
+							queue_t.push(' ');
+						}
+						stack_s.push(*p);
+					}
+				}
+			}
+		}
+		++p;
+	}
+	while(!stack_s.empty()){
+		queue_t.push(stack_s.top());
+		stack_s.pop();
+		queue_t.push(' ');
+	}
+}
+void output(){
+	cout<<"Reverse Polish Notation：";
+	while(!queue_t.empty()) {
+			cout<<queue_t.front();
+			queue_t.pop();
+	}
+	cout<<endl;
+}
+
+int main(){
+	string input_s;
+	input_s = input();
+	const char *expr = input_s.c_str();
+	change(expr);
+	output();
+}
+
